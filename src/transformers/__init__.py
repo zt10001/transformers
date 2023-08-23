ff4808055a995925de0a3a2b6673a947704c98ce
Matt <rocketknight1@gmail.com>
2023-08-23 15:34:16 +0100

    Everything replaced with pure templating

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -669,7 +669,6 @@ _import_structure = {
         "pipeline",
     ],
     "processing_utils": ["ProcessorMixin"],
-    "prompt_utils": ["PromptConfig"],
     "testing_utils": [],
     "tokenization_utils": ["PreTrainedTokenizer"],
     "tokenization_utils_base": [
@@ -4708,7 +4707,6 @@ if TYPE_CHECKING:
         pipeline,
     )
     from .processing_utils import ProcessorMixin
-    from .prompt_utils import PromptConfig
 
     # Tokenization
     from .tokenization_utils import PreTrainedTokenizer
