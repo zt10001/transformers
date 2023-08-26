0adb31ff87d6a4aed319c88773a9ecdfafce37d4
ydshieh <ydshieh@users.noreply.github.com>
2023-08-26 08:17:12 +0200

    temp

--- src/transformers/models/kosmos2/modeling_kosmos2.py
+++ src/transformers/models/kosmos2/modeling_kosmos2.py
@@ -105,6 +105,18 @@ def create_position_ids_from_input_ids(input_ids, padding_idx, past_key_values_l
 
 
 KOSMOS2_START_DOCSTRING = r"""
+    This model inherits from [`PreTrainedModel`]. Check the superclass documentation for the generic methods the
+    library implements for all its model (such as downloading or saving, resizing the input embeddings, pruning heads
+    etc.)
+
+    This model is also a PyTorch [torch.nn.Module](https://pytorch.org/docs/stable/nn.html#torch.nn.Module) subclass.
+    Use it as a regular PyTorch Module and refer to the PyTorch documentation for all matter related to general usage
+    and behavior.
+
+    Parameters:
+        config ([`BlipConfig`]): Model configuration class with all the parameters of the model.
+            Initializing with a config file does not load the weights associated with the model, only the
+            configuration. Check out the [`~PreTrainedModel.from_pretrained`] method to load the model weights.
 """
 
 KOSMOS2_VISION_INPUTS_DOCSTRING = r"""
