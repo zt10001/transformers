5d03b0ba95ab1663321561ac14e58212afd989fc
ydshieh <ydshieh@users.noreply.github.com>
2023-08-26 08:25:20 +0200

    temp

--- src/transformers/models/kosmos2/modeling_kosmos2.py
+++ src/transformers/models/kosmos2/modeling_kosmos2.py
@@ -1485,6 +1485,11 @@ class Kosmos2TextForCausalLM(Kosmos2PreTrainedModel):
         return_dict: Optional[bool] = None,
     ) -> Union[Tuple, CausalLMOutputWithCrossAttentions]:
         r"""
+        labels (`torch.LongTensor` of shape `(batch_size, sequence_length)`, *optional*):
+            Labels for computing the left-to-right language modeling loss (next word prediction). Indices should be in
+            `[-100, 0, ..., config.vocab_size]` (see `input_ids` docstring) Tokens with indices set to `-100` are
+            ignored (masked), the loss is only computed for the tokens with labels in `[0, ..., config.vocab_size]`
+
         Returns:
 
         """
@@ -1763,6 +1768,11 @@ class Kosmos2ForConditionalGeneration(Kosmos2PreTrainedModel):
         return_dict: Optional[bool] = None,
     ) -> Union[Tuple, Kosmos2ForConditionalGenerationModelOutput]:
         r"""
+        labels (`torch.LongTensor` of shape `(batch_size, sequence_length)`, *optional*):
+            Labels for computing the left-to-right language modeling loss (next word prediction). Indices should be in
+            `[-100, 0, ..., config.vocab_size]` (see `input_ids` docstring) Tokens with indices set to `-100` are
+            ignored (masked), the loss is only computed for the tokens with labels in `[0, ..., config.vocab_size]`
+
         Returns:
 
         Examples:
